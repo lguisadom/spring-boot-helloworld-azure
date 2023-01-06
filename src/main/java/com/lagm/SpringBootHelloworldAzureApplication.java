e6bcf823bacb3a0699751c9059a1005fe37b989a
Luis Guisado <luis.guisado.matrix@gmail.com>
2023-01-06 00:14:50 -0500

    Initial commit

---
+++ src/main/java/com/lagm/SpringBootHelloworldAzureApplication.java
@@ -0,0 +1,13 @@
+package com.lagm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloworldAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloworldAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lagm/SpringBootHelloworldAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloworldAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
